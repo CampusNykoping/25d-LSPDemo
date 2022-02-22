393b7bb661ae2692fdbd075485e8381d2c802159
Claes Engelin <claes.engelin@zocom.se>
2022-02-22 16:25:50 +0100

    Add project files.

---
+++ DemoLibrary/Manager.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoLibrary;
+
+public class Manager : Employee
+{
+    public override void CalculatePerHourRate(int rank)
+    {
+        decimal baseAmount = 19.75M;
+        Salary = baseAmount + (rank * 4);
+    }
+
+    public void GeneratePerformanceReview()
+    {
+        // Simulate reviewing a direct report
+        Console.WriteLine("I am reviewing a direct report's performance.");
+    }
+}
